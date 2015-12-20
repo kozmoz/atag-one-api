802cb3262dd5c2c155501d8797707e7f75b058c5
Rob Juurlink <git@juurlink.org>
2015-12-21 00:38:48 +0100

    Generate changelog automatically

---
+++ generate_changelog.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+#
+# Generate changelog automatically.
+# .github_changelog_generator contains configuration.
+#
+# See for details: https://github.com/skywinder/Github-Changelog-Generator
+github_changelog_generator
