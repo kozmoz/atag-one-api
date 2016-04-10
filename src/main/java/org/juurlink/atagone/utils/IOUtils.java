7f88c35844a95cb7b249a3bab784709ce8ebabe1
Rob Juurlink <git@juurlink.org>
2016-04-10 23:06:55 +0200

    Fix for running with Java 6 (IncompatibleClassChangeError, DatagramSocket does not implement interface Closeable). Fixes #6

--- src/main/java/org/juurlink/atagone/utils/IOUtils.java
+++ src/main/java/org/juurlink/atagone/utils/IOUtils.java
@@ -6,6 +6,7 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.UnsupportedEncodingException;
+import java.net.DatagramSocket;
 
 import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
@@ -19,43 +20,49 @@ import lombok.experimental.UtilityClass;
 @UtilityClass
 public class IOUtils {
 
-	/**
-	 * Close stream, ignore all errors.
-	 */
-	public static void closeQuietly(@Nullable final Closeable closeable) {
-		if (closeable != null) {
-			try {
-				closeable.close();
-			} catch (IOException e) {
-				//Ignore
-			}
-		}
-	}
+    /**
+     * Close stream, ignore all errors.
+     */
+    public static void closeQuietly(@Nullable final Closeable closeable) {
+        if (closeable == null) {
+            return;
+        }
+        try {
+            // Prevent Class java.net.DatagramSocket does not implement the requested interface java.io.Closeable when Java < 7.
+            if (closeable instanceof DatagramSocket) {
+                ((DatagramSocket) closeable).close();
+            } else {
+                closeable.close();
+            }
+        } catch (IOException e) {
+            //Ignore
+        }
+    }
 
-	/**
-	 * Read full stream to String.
-	 *
-	 * @param stream   Input Stream or null
-	 * @param encoding Character encoding, ie UTF-8
-	 * @throws UnsupportedEncodingException If the named charset is not supported
-	 */
-	@Nonnull
-	public static String toString(@Nullable final InputStream stream, @Nonnull @NonNull final String encoding) throws IOException {
-		if (stream == null) {
-			return "";
-		}
+    /**
+     * Read full stream to String.
+     *
+     * @param stream   Input Stream or null
+     * @param encoding Character encoding, ie UTF-8
+     * @throws UnsupportedEncodingException If the named charset is not supported
+     */
+    @Nonnull
+    public static String toString(@Nullable final InputStream stream, @Nonnull @NonNull final String encoding) throws IOException {
+        if (stream == null) {
+            return "";
+        }
 
-		BufferedReader reader = null;
-		try {
-			reader = new BufferedReader(new InputStreamReader(stream, encoding));
-			StringBuilder out = new StringBuilder();
-			String line;
-			while ((line = reader.readLine()) != null) {
-				out.append(line);
-			}
-			return out.toString();
-		} finally {
-			closeQuietly(reader);
-		}
-	}
+        BufferedReader reader = null;
+        try {
+            reader = new BufferedReader(new InputStreamReader(stream, encoding));
+            StringBuilder out = new StringBuilder();
+            String line;
+            while ((line = reader.readLine()) != null) {
+                out.append(line);
+            }
+            return out.toString();
+        } finally {
+            closeQuietly(reader);
+        }
+    }
 }
